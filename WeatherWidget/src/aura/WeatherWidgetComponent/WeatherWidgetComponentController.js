87a68f756d5532e8f6cf4b6fd38d8178344256e0
DarkAl550 <alxandr.black@gmail.com>
2020-11-27 00:45:41 +0300

    Create and test WeatherWidget

--- WeatherWidget/src/aura/WeatherWidgetComponent/WeatherWidgetComponentController.js
+++ WeatherWidget/src/aura/WeatherWidgetComponent/WeatherWidgetComponentController.js
@@ -1,5 +1,6 @@
 ({
-    myAction : function(component, event, helper) {
-
+    doInit : function(component, event, helper) {
+        var city = component.get("v.cityName");
+        helper.getWeatherParams(component, event);
     }
 })
